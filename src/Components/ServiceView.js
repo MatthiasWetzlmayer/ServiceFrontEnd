de9ef82ccf2ba069e00b034b401e6f7c9fda641e
Matthias Wetzlmayer <mwetzl@gmail.com>
2020-04-05 14:02:34 +0200

    Service-Components added

---
+++ src/Components/ServiceView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ServiceView extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ServiceView
