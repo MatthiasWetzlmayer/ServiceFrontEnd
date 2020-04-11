ef6e9561c3240f8c6ac3622b9704760bff325b19
daniel-riffi <riffert.daniel@gmail.com>
2020-04-11 15:38:38 +0200

    implemented employeeView

--- src/Components/EmployeeView.js
+++ src/Components/EmployeeView.js
@@ -1,14 +1,164 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { view } from '@risingstack/react-easy-state';
+import EmployeeTable from './EmployeeTable';
+import employees from '../States/EmployeeState';
+import App from '../App.css';
 
 export class EmployeeView extends Component {
+
+  state = {
+    pageNr: 1
+  }
+
+  componentDidMount() {
+    employees.initalize();
+    employees.min = 1;
+    employees.showEntries = document.getElementById("entries").value;
+    employees.loadEmployees();
+    employees.max = employees.showEntries;
+  }
+
+  selectionChanged = (e) => {
+    e.preventDefault();
+    employees.showEntries = e.target.value;
+    employees.min = 1;
+    employees.max = employees.showEntries;
+    employees.loadEmployees();
+    this.setState({ pageNr: 1 });
+  }
+
+  searchChanged = (e) => {
+    e.preventDefault();
+    employees.filterEmployees(e.target.value);
+  }
+
+  nextButtonClicked = (e) => {
+    e.preventDefault();
+    employees.min = parseInt(employees.min) + parseInt(employees.showEntries);
+    employees.max = parseInt(employees.max) + parseInt(employees.showEntries);
+    employees.loadEmployees();
+    this.setState({ pageNr: this.state.pageNr + 1 });
+  }
+
+  previousButtonClicked = (e) => {
+    e.preventDefault();
+    if (employees.max == employees.nrAllEmployees) {
+      employees.max = parseInt(employees.max) - parseInt(employees.nrAllEmployees % employees.showEntries)
+    }
+    else {
+      employees.max = parseInt(employees.max) - parseInt(employees.showEntries);
+    }
+    employees.min = parseInt(employees.min) - parseInt(employees.showEntries);
+    employees.loadEmployees();
+    this.setState({ pageNr: this.state.pageNr - 1 });
+  }
+
+  addEmployee = () => {
+    employees.showAddEmployee = !employees.showAddEmployee;
+  }
+
   render() {
     return (
-      <div>
-        
+      <div >
+        <header style={headerStyle}>
+
+          <div style={textStyle}>Show</div>
+          <div>
+            <select
+              name="entries"
+              id="entries"
+              onChange={this.selectionChanged}
+              style={colorStyle}>
+              <option value="2">2</option>
+              <option value="10">10</option>
+              <option value="25">25</option>
+              <option value="null">All</option>
+            </select>
+          </div>
+          <div style={textStyle}>entries</div>
+
+          <button
+            style={specialButtonStyle}
+            onClick={this.addEmployee}
+            className="button">
+            {employees.showAddEmployee ? "Mitarbeiter anlegen abbrechen" : "Mitarbeiter anlegen"}
+          </button>
+
+          <div style={rightStyle}>
+            <label for="search">Search: </label>
+            <input
+              name="search"
+              id="search"
+              type="text"
+              style={colorStyle}
+              onChange={this.searchChanged}></input>
+          </div>
+
+        </header>
+        <br></br>
+        <br></br>
+        <br></br>
+        <footer style={footerStyle}>
+          <div style={textStyle}>Showing</div>
+          <div>{employees.min}</div>
+          <div style={textStyle}>to</div>
+          <div>{employees.max}</div>
+          <div style={textStyle}>of</div>
+          <div>{employees.nrAllEmployees}</div>
+          <div style={textStyle}>entries</div>
+
+          <div style={footerRightStyle}>
+            <button id="previous" disabled={employees.min == 1} className={employees.min == 1 ? "button disabled" : "button"} onClick={this.previousButtonClicked}>Previous</button>
+            <div id="pageNr" style={textStyle}>{this.state.pageNr}</div>
+            <button id="next" disabled={employees.max == employees.nrAllEmployees} className={employees.max == employees.nrAllEmployees ? "button disabled" : "button"} onClick={this.nextButtonClicked}>Next</button>
+          </div>
+        </footer>
       </div>
     )
   }
+}
+
+const footerStyle = {
+  borderTop: '2px solid #888',
+
+  display: 'flex',
+  alignItems: 'center',
+  background: '#ccc'
+}
+
+const headerStyle = {
+  borderBottom: '2px solid #888',
+
+  display: 'flex',
+  alignItems: 'center',
+  background: '#ccc'
+}
+
+const rightStyle = {
+  paddingRight: '10px',
+  marginLeft: 'auto'
+}
+
+const footerRightStyle = {
+  padding: '10px',
+  marginLeft: 'auto',
+
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'row'
+}
+
+const textStyle = {
+  padding: '10px'
+}
+
+const colorStyle = {
+  background: 'rgb(129, 129, 129)',
+  color: '#fff'
+}
+
+const specialButtonStyle = {
+  marginLeft: 'auto'
 }
 
 export default view(EmployeeView)
--- src/States/EmployeeState.js
+++ src/States/EmployeeState.js
@@ -7,14 +7,41 @@ const privateVars = {
     allEmployees: [],
 }
 
-const employeeState=store({
+const employees = store({
     employees: [],
-    employeeToEdit:{},
-    addEmployee:()=>{},
-    deleteEmployee:()=>{},
-    setEmployeeToEdit:()=>{},
-    editEmployee:()=>{},
-    loadEmployees:()=>{},
-    filterEmployees: () => {}
+    employeeToEdit: {},
+    min: 0,
+    max: 0,
+    showEntries: 0,
+    nrAllEmployees: 0,
+    showAddEmployee: false,
+    addEmployee: () => { },
+    deleteEmployee: () => { },
+    setEmployeeToEdit: () => { },
+    editEmployee: () => { },
+    loadEmployees: () => {
+        var result = dataService.loadEmployees(employees.min, employees.max);
+        employees.employees = result;
+        if (employees.max > employees.employees.length || employees.max == "null") {
+            employees.max = employees.employees.length;
+        }
+    },
+    filterEmployees: (searchString) => {
+        if (privateVars.allEmployees === null) {
+            privateVars.allEmployees = employees.employees;
+        }
+        if (searchString.length < 1) {
+            employees.employees = privateVars.allEmployees;
+            privateVars.allEmployees = null;
+        }
+        else {
+            employees.employees = employees.employees.filter(x => x.name.startsWith(searchString));
+        }
+    },
+
+    initalize: () => {
+        var result = dataService.size();
+        employees.nrAllEmployees = result;
+    },
 });
-export default employeeState
+export default employees
